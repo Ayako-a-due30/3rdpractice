d73dba89524f81be641c420de6e3b1da645f5494
Ayako-a-due30 <114543625+Ayako-a-due30@users.noreply.github.com>
2022-12-02 08:54:52 +0900

    index.phpの修正

--- src/app/index.php
+++ src/app/index.php
@@ -4,14 +4,13 @@ require('../function/function.php');
 require('../function/auth.php');
 
 $currentPageNum = (!empty($_GET['p']))?$_GET['p']:1;
-// if(!is_int(int)$currentPageNum)
-// {
-//     header("Location:index.php");
-// }
+if(!is_int((int)$currentPageNum)){
+    header("Location:index.php");
+}
 //表示件数
 $listSpan=3;
 $currentMinNum = (($currentPageNum-1)*$listSpan);
-// $dbProductData = getProductList($currentMinNum);
+$dbProductData = getProductList($currentMinNum);
 $dbCategoryData = getCategory();
 ?>
 
@@ -23,7 +22,7 @@ $dbCategoryData = getCategory();
     <title>記事一覧</title>
 </head>
 <body>
-    <!-- <?php echo $dbProductData;?> -->
+    <?php echo $dbProductData;?>
     <section id= "main">
 
     </section>
